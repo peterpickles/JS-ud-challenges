f915384cd9a41568c68f15ab2e1559722c5dfbfd
peterpickles <lipeter2006@gmail.com>
2018-07-19 00:00:44 -0700

    adding to operators include logical operators

--- basicOperators.js
+++ basicOperators.js
@@ -1,8 +1,15 @@
 //Basic operators
 
 var year, yearJohn, yearMark;
-year = 2020;
-yearJohn = year - 28;
-yearMark = year - 23;
+year = 2018;
+yearJohn = now - 28;
+yearMark = now - 33;
 
 console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical Operators
+
